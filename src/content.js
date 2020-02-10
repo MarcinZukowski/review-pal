45f7cf77a0656fa0c2e3be88e5ec0841b1d28d3f
Marcin Zukowski <marcin.zukowski@gmail.com>
2020-02-10 10:53:36 -0800

    Some changes from many months ago I forgot to push

--- src/content.js
+++ src/content.js
@@ -55,6 +55,11 @@ class CDMDiff
         let [left, right] = CDMDiff.getRowRanges(row);
         return CDMDiff.createId(left, right);
     }
+    getNumLines()
+    {
+        return (this.left_from  > 0 ? 1 + this.left_to  - this.left_from  : 0)
+             + (this.right_from > 0 ? 1 + this.right_to - this.right_from : 0);
+    }
 }
 
 /* Main class */
@@ -71,13 +76,37 @@ class CDM
         let greenUrl = chrome.runtime.getURL("images/green-128.png");
         this.redHTML = `src="${redUrl}" alt="(_)" `;
         this.greenHTML = `src="${greenUrl}" alt="(X)" `;
+
+//        this.middleClick = this.middleClick.bind(this);
     }
 
     start()
     {
         $(window).on('hashchange', this.hashChanged.bind(this));
         $(window).on("mousedown", this.middleClick.bind(this));
+        $(window).on("keypress", this.keyPressed.bind(this));
         this.hashChanged();
+    }
+
+    keyPressed(e)
+    {
+        if (!e.shiftKey) {
+            return true;
+        }
+        if (e.key === "K") {
+            this.gotoNext(undefined);
+            return true;
+        }
+        if (e.key === "X") {
+            if (this.lastFoundDiffIdx >= 0)
+            {
+                this.flip(this.diffs[this.lastFoundDiffIdx]);
+            }
+            else
+            {
+                this.message("No diff selected");
+            }
+        }
     }
 
     findDiffWith(left, right)
@@ -142,6 +171,9 @@ class CDM
     {
         this.href = window.location.href;
         this.dataKey = "cdm::" + this.href;
+        this.lastFoundDiffIdx = -1;
+
+        this.cleanData();
 
         let hash = window.location.hash;
         console.log("hashchanged: " + hash);
@@ -225,6 +257,12 @@ class CDM
     initDataLoad()
     {
         chrome.storage.local.get([this.dataKey], this.dataLoaded.bind(this));
+    }
+
+    cleanData()
+    {
+        this.diffs = [];
+        this.data = null;
     }
 
     dataLoaded(result)
@@ -320,7 +358,14 @@ class CDM
 
     analyzeDiffs()
     {
+        if (!this.data) {
+            // Sometimes we get here when the data is not ready yet.
+            return;
+        }
         this.unified = this.isUnified();
+
+        // Clean previously set breaks
+//        $(".cdm-forcedBreak").removeClass("cdm-forcedBreak");
 
         let diffStart = $("#diffStart" + this.id);
 
@@ -373,17 +418,28 @@ class CDM
         let done = this.data.done.length;
         let todo = total - done;
 
+        let totalLines = 0, doneLines = 0;
+        for (let i = 0; i < this.diffs.length; i++)
+        {
+            totalLines += this.diffs[i].getNumLines();
+            if (this.isDone(this.diffs[i].getId()))
+            {
+                doneLines += this.diffs[i].getNumLines();
+            }
+        }
+        let todoLines = totalLines - doneLines;
+
         $("#" + this.statsId).html(`
-${this.LABEL}: &nbsp; ${total} diffs in total. 
+${this.LABEL}: &nbsp; ${total} diffs (${totalLines} lines) in total. 
 &nbsp;&nbsp;
   <span class="cdm-stats-text cdm-stats-done">
     <img ${this.greenHTML} width="16" height="16"/>
-    ${done} diffs done.
+    ${done} diffs (${doneLines} lines) done.
   </span>
 &nbsp;&nbsp;
   <span class="cdm-stats-text cdm-stats-todo">
     <img ${this.redHTML} width="16" height="16"/> 
-    ${todo} diffs to do.
+    ${todo} diffs (${todoLines} lines) to do.
   </span>
 `);
 
@@ -406,21 +462,33 @@ ${this.LABEL}: &nbsp; ${total} diffs in total.
         $(".cdm-stats-done").click(this.gotoNext.bind(this, true));
     }
 
+    /**
+     *  @param shouldBeDone  If defined, only diffs that are done (true) or not done (false)
+     *                       are chosen.
+     *                       If undefined, all diffs are chosen.
+     */
     gotoNext(shouldBeDone)
     {
+        $(".cdm-jump").removeClass("cdm-jump");
+
+        let startIdx = this.lastFoundDiffIdx;
+
         let container = $("#sourceTable" + this.id);
         let containerOffset = container.offset().top - container.scrollTop();
         let currentTop = $(window).scrollTop();
         let foundDiff = null;
         let foundTop = 0;
-        for (let d = 0; d < this.diffs.length; d++) {
-            let diff = this.diffs[d];
-            if (this.isDone(diff.getId()) === shouldBeDone) {
+        let count = this.diffs.length;
+        for (let d = 1; d < count; d++) {
+            let idx = (d + startIdx) % count;
+            let diff = this.diffs[idx];
+            if (shouldBeDone === undefined || this.isDone(diff.getId()) === shouldBeDone) {
                 let top = diff.rows[0].offset().top - containerOffset;
                 // Accept first matching row, or first matching after the current window position.
                 if (foundDiff === null || top > currentTop) {
                     foundDiff = diff ;
                     foundTop = top;
+                    this.lastFoundDiffIdx = idx;
                     if (top > currentTop) {
                         break;
                     }
@@ -429,13 +497,16 @@ ${this.LABEL}: &nbsp; ${total} diffs in total.
         }
         if (foundDiff !== null) {
             // Found something, scroll there
-            this.message(`Scrolling to ${foundDiff.getId()}`);
+            let prevTop = $(window).scrollTop();
             $(window).scrollTop(foundTop);
+            let currTop = $(window).scrollTop();
+            this.message(`Scrolling to ${foundDiff.getId()} (${prevTop} -> ${foundTop} -> ${currTop})`);
             // Add animation and removal of it after it's done
             foundDiff.rows[0].addClass("cdm-jump");
-            window.setTimeout(function() { this.removeClass("cdm-jump"); }.bind(foundDiff.rows[0]), 500);
+//            window.setTimeout(function() { this.removeClass("cdm-jump"); }.bind(foundDiff.rows[0]), 500);
         } else {
             this.message(`No ${shouldBeDone? "" : "un"}reviewed diffs!`);
+            // this.lastFoundDiffIdx = -1;
         }
         return false;
     }
@@ -500,4 +571,3 @@ ${this.LABEL}: &nbsp; ${total} diffs in total.
 
 let cdm = new CDM();
 cdm.start();
-
